49df09bbb776d56844a1cc055f2973c73fa38f80
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-06-26 16:01:56 +0200

    Implement integration using Airtable SDK

--- index.js
+++ index.js
@@ -1,25 +1,117 @@
-const { OPTIMIZELY_API_TOKEN } = process.env;
+// Imports
+const airtable = require('airtable');
+
+// Secret constants
+const { AIRTABLE_API_TOKEN, OPTIMIZELY_API_TOKEN } = process.env;
+
+// Fixed IDs within Airtable
+const BASE_ID = 'apprz55Uggo1JWTSV';
+const TABLE_NAME = 'Results export';
 
 // Base URL of Optimizely API
-const baseUrl = 'https://api.optimizely.com/v2';
+const OPTIMIZELY_API_BASE_URL = 'https://api.optimizely.com/v2';
 
-// Authentication headers for API
-const defaultHeaders = {
+// Authentication headers for Optimizely API
+const OPTIMIZELY_API_DEFAULT_HEADERS = {
   headers: {
     Authorization: `Bearer ${OPTIMIZELY_API_TOKEN}`,
   },
 };
 
-// Get a dictionary which maps Optimizely experiment IDs to Airtable record IDs
-const getAirtableRecords = async (table) => {
-  const response = await table.selectRecordsAsync({
-    fields: ['ID']
-  });
+// Authentication for Airtable SDK
+airtable.configure({ apiKey: AIRTABLE_API_TOKEN })
+const table = airtable.base(BASE_ID)(TABLE_NAME);
 
-  return Object.fromEntries(response.records.map((record) => {
-    return [record.getCellValueAsString('ID'), record.id]
-  }));
-}
+// Get a dictionary which maps Optimizely experiment IDs to Airtable record IDs
+const getAirtableRecords = () => {
+  // Return a Promise so we can await the response
+  return new Promise((resolve, reject) => {
+    // Start with empty map
+    let result = {};
+
+    // NOTE: I don't understand how to get pagination working
+    table.select({
+        fields: ['ID']
+    }).eachPage((records, next) => {
+
+      // Add each record on current page to the map
+      records.forEach((record) => {
+        const recordId = record.id;
+        const experimentId = parseInt(record.fields['ID']);
+        result[experimentId] = recordId;
+      });
+
+      next();
+
+    }, (error) => {
+        // Resolve promise
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+    });
+  });
+};
+
+const createAirtableRecord = (fields) => {
+  // Return a Promise so we can await the response
+  return new Promise((resolve, reject) => {
+    // Create new record
+    table.create([
+      { fields }
+    ], {
+      typecast: true
+    }, (error, result) => {
+      // Resolve promise
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+const updateAirtableRecord = (id, fields) => {
+  // Return a Promise so we can await the response
+  return new Promise((resolve, reject) => {
+    // Create new record
+    table.update([
+      { id, fields }
+    ], {
+      typecast: true
+    }, (error, result) => {
+      // Resolve promise
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+const deleteAirtableRecords = (ids) => {
+  // Return a Promise so we can await the response
+  return new Promise((resolve, reject) => {
+    // No need to make request when nothing to delete
+    if (ids.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    // Create new record
+    table.destroy(ids, (error, result) => {
+      // Resolve promise
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
 
 // Combine project, experiment and results into Airtable fields
 const buildAirtableFields = (project, experiment, results) => {
@@ -36,8 +128,8 @@ const buildAirtableFields = (project, experiment, results) => {
     const primaryMetric = results.metrics[0];
 
     fields = {
-      'Start': new Date(results.start_time),
-      'End': new Date(results.end_time),
+      'Start': results.start_time,
+      'End': results.end_time,
       'Primary Metric': primaryMetric.name,
       ...fields
     };
@@ -61,6 +153,12 @@ const buildAirtableFields = (project, experiment, results) => {
     // Add even more additional fields from treatment uplift
     if (winningVariation && winningVariation.lift) {
       const { lift } = winningVariation;
+
+      // Don't just show MAX INT
+      if (lift.visitors_remaining === 9223372036854776000) {
+        lift.visitors_remaining = null;
+      }
+
       fields = {
         'Improvement': lift.value,
         'Statistical Significance': lift.significance,
@@ -79,12 +177,12 @@ const buildAirtableFields = (project, experiment, results) => {
   }
 
   return fields;
-}
+};
 
 // Helper function for making request to Optimizely API
 const optimizelyApi = async (path) => {
-  const url = `${baseUrl}${path}`;
-  const response = await fetch(url, defaultHeaders);
+  const url = `${OPTIMIZELY_API_BASE_URL}${path}`;
+  const response = await fetch(url, OPTIMIZELY_API_DEFAULT_HEADERS);
 
   // Optimizely API might return 204 No Content
   if (response.status === 204) {
@@ -92,39 +190,33 @@ const optimizelyApi = async (path) => {
   } else {
     return await response.json();
   }
-}
+};
 
 // Get list of all active projects
-const getProjects = () => {
+const getOptimizelyProjects = () => {
   return optimizelyApi('/projects');
-}
+};
 
 // Get experiments in project
-const getExperiments = (projectId) => {
+const getOptimizelyExperiments = (projectId) => {
   // NOTE: might need to implement pagination
   return optimizelyApi(`/experiments?project_id=${projectId}&per_page=100`);
-}
+};
 
 // Get results of an experiment
-const getResults = (experimentId) => {
+const getOptimizelyResults = (experimentId) => {
   return optimizelyApi(`/experiments/${experimentId}/results`);
-}
+};
 
 // Main function
 const handler = async () => {
-  console.log('Start fetching experiment data...');
-
-  // // Get Airtable table
-  // const tableName = 'Results export';
-  // const table = base.getTable(tableName);
+  console.log('Starting import...');
 
   // // Get current records in Airtable
-  // let records = await getAirtableRecords(table);
-  // console.log(`Found ${Object.keys(records).length} records in Airtable`);
-  let records = {};
+  let records = await getAirtableRecords(table);
 
   // Get all projects
-  const projects = await getProjects();
+  const projects = await getOptimizelyProjects();
 
   // Iterate over all projects asynchronously
   await Promise.all(projects.map(async (project) => {
@@ -135,7 +227,7 @@ const handler = async () => {
     }
 
     // Get all experiments
-    const experiments = await getExperiments(project.id);
+    const experiments = await getOptimizelyExperiments(project.id);
 
     // Iterate over all experiments asynchronously
     await Promise.all(experiments.map(async (experiment) => {
@@ -149,38 +241,32 @@ const handler = async () => {
       // Get results for experiment only if the experiment is started
       let results = null;
       if (experiment.status !== 'not_started') {
-        results = await getResults(experiment.id);
+        results = await getOptimizelyResults(experiment.id);
       }
 
       // Combine all data into fields for Airtable
       const fields = buildAirtableFields(project, experiment, results);
 
-      console.log(fields);
+      if (experiment.id in records) {
+        // Update existing record
+        const recordId = records[experiment.id];
+        await updateAirtableRecord(recordId, fields);
 
-      // if (experiment.id in records) {
-      //   // Update existing record
-      //   const recordId = records[experiment.id];
-      //   await table.updateRecordAsync(recordId, fields);
-
-      //   // Mark as done
-      //   records[experiment.id] = null;
-      // } else {
-      //   // Insert new record
-      //   await table.createRecordAsync(fields);
-      // }
+        // Mark as done
+        delete records[experiment.id];
+      } else {
+        // Insert new record
+        await createAirtableRecord(fields);
+      }
     }));
   }));
 
   // Remove records that no longer exist
   console.log('Cleaning up...');
-  await Promise.all(Object.values(records).map(async (recordId) => {
-    if (recordId) {
-      await table.deleteRecordAsync(recordId);
-    }
-  }));
+  await deleteAirtableRecords(Object.values(records));
 
-  console.log('Operation complete!');
-}
+  console.log('Import completed!');
+};
 
 if (require.main === module) {
   handler();
